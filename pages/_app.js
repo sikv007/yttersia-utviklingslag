676bdcdf234c5f6b76d396e6f5cb1c6d07ca48cc
Sigurd Kvamme <sigurdkvamme@live.no>
2022-07-20 01:03:54 +0200

    updated eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/globals.css';
-import { Header } from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
       <Component {...pageProps} />
     </>
   );
